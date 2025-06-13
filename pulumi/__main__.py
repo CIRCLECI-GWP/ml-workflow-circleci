2bde8ec5666a8307a367108cca087f8c2942fa4b
yemiwebby <yemiwebby@gmail.com>
2025-06-13 14:28:36 +0100

    update

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -5,10 +5,10 @@ runnerPublicIp = scaleway.InstanceIp("runnerPublicIp")
 serverPublicIp = scaleway.InstanceIp("serverPublicIp")
 
 modelTrainingCCIRunner = scaleway.InstanceServer(
-    "runnerServerLinuxGPU",
+    "runnerServerLinux",
     zone="fr-par-2",
-    type="PRO2-M",
-    image="34e2e0d1-899e-40a7-9f08-a75ab3c59fb8",
+    type="DEV1-S",  # Change to a type you have quota for
+    image="34e2e0d1-899e-40a7-9f08-a75ab3c59fb8",  # Standard Ubuntu 24.04 x86_64 image
     ip_id=runnerPublicIp.id,
     routed_ip_enabled=True,
     root_volume=scaleway.InstanceServerRootVolumeArgs(
@@ -23,8 +23,8 @@ modelTrainingCCIRunner = scaleway.InstanceServer(
 tensorflowServer = scaleway.InstanceServer(
     "tensorflowServerLinux",
     zone="fr-par-2",
-    type="PRO2-M",
-    image="34e2e0d1-899e-40a7-9f08-a75ab3c59fb8",
+    type="DEV1-S",  # or any CPU type you have quota for
+    image="34e2e0d1-899e-40a7-9f08-a75ab3c59fb8",  # Ubuntu 24.04 x86_64
     ip_id=serverPublicIp.id,
     routed_ip_enabled=True,
     root_volume=scaleway.InstanceServerRootVolumeArgs(
