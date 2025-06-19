8b6b750d74cc5782749b642535b159044adf1668
yemiwebby <yemiwebby@gmail.com>
2025-06-19 21:20:34 +0100

    update pulumi

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,47 +1,46 @@
 import pulumi
 import lbrlabs_pulumi_scaleway as scaleway
 
+import pulumi
+import pulumiverse_scaleway as scaleway
+from pulumiverse_scaleway.instance import Server, Ip, get_image
+
 zone = "fr-par-1"
 
 # Reserve two public IPs
-runner_ip = scaleway.InstanceIp("runnerPublicIp", zone=zone)
-server_ip = scaleway.InstanceIp("serverPublicIp", zone=zone)
+runner_ip = Ip("runnerPublicIp", zone=zone)
+server_ip = Ip("serverPublicIp", zone=zone)
 
 # SBS-backed Ubuntu Jammy image (looked up manually)
-JAMMY_SBS_ID = "e17b585e-c52f-44b0-97f6-07c18bb5bb86"
+# JAMMY_SBS_ID = "e17b585e-c52f-44b0-97f6-07c18bb5bb86"
+jammy = get_image(label="ubuntu_jammy", zone=zone, volume_type="b_ssd")
 
 # GPU runner
-modelTrainingCCIRunner = scaleway.InstanceServer(
+modelTrainingCCIRunner = Server(
     "runnerServerLinux",
     zone=zone,
     type="GP1-XS",
-    image=JAMMY_SBS_ID,
+    image=jammy.id,
     ip_id=runner_ip.id,
-    routed_ip_enabled=True,
-    root_volume=scaleway.InstanceServerRootVolumeArgs(
-        size_in_gb=80,
-        volume_type="b_ssd",
-    ),
-    user_data={
-        "cloud-init": open("runner_cloud_init.yml").read(),
+    root_volume={
+        "size_in_gb": 80,
+        "volume_type":"b_ssd",
     },
+    cloud_init=open("runner_cloud_init.yml").read(),
 )
 
 # CPU model server
-tensorflowServer = scaleway.InstanceServer(
+tensorflowServer = Server(
     "tensorflowServerLinux",
     zone=zone,
     type="DEV1-L",
-    image=JAMMY_SBS_ID,
+    image=jammy.id,
     ip_id=server_ip.id,
-    routed_ip_enabled=True,
-    root_volume=scaleway.InstanceServerRootVolumeArgs(
-        size_in_gb=40,
-        volume_type="b_ssd",
-    ),
-    user_data={
-        "cloud-init": open("modelserver_cloud_init.yml").read(),
+    root_volume={
+        "size_in_gb": 40,
+        "volume_type": "b_ssd",
     },
+    cloud_init=open("modelserver_cloud_init.yml").read(),
 )
 
 # Export outputs
